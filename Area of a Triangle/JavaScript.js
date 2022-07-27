d4d936bdf59e594c0cd7fa4731c8212f85337c35
Elena Kuparadze <elenakuparadze@gmail.com>
2022-07-28 00:08:34 +0400

    init: exercise - Area of a Triangle

---
+++ Area of a Triangle/JavaScript.js
@@ -0,0 +1,16 @@
+function triArea(base, height) {
+  const area = (base / 2) * height;
+  return area;
+}
+
+const result1 = triArea(3, 2);
+const result2 = triArea(5, 4);
+const result3 = triArea(10, 10);
+const result4 = triArea(0, 60);
+const result5 = triArea(12, 11);
+
+console.log(result1);
+console.log(result2);
+console.log(result3);
+console.log(result4);
+console.log(result5);
